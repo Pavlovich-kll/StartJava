20e24632c925f5f336e7f9904b00a31ea9d154e9
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-09 19:27:15 +0300

    перепутал папки

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,21 +1,37 @@
 public class Calculator {
-	public static void main(String[] args) {
-		int a = 10;
-		int b = 5;
-		char action = '^';
 
-		if(action == '+') {
-			System.out.println("Результат сложения = " + (a + b));
-		} else if(action == '-') {
-			System.out.println("Результат вычитания = " + (a - b));
-		} else if(action == '*') {
-			System.out.println("Результат умножения = " + (a * b));
-		} else if(action == '/') {
-			System.out.println("Результат деления = " + (a / b));
-		} else if(action == '%') {
-			System.out.println("Результат деления по модулю = " + (a % b));
-		} else if(action == '^') {
-			System.out.println("Результат возведения в степень = " + Math.pow(a, b));
+int number1;
+int number2;
+int result;
+char mathAction;
+
+switch (mathAction) {
+	case '+': mathAction = '+';
+	System.out.println(+ result = (number1 + number2));
+	break;
+
+	case '-': mathAction = '-';
+	System.out.println(+ result = (number1 - number2));
+	break;
+
+	case '*': mathAction = '*';
+	System.out.println(+ result = (number1 * number2))
+	break;
+
+	case '/': mathAction = '/';
+	System.out.println(+ result = (number1 / number2))
+	break;
+
+	case '%': mathAction = '%';
+	System.out.println(+ result = (number1 % number2));
+	break;
+
+	case '^': mathAction = '^';
+	int c = 1;
+		for(int i = 1; i <= number2; i++) {
+			c *= number1;
 		}
+	System.out.println(+ result = c);
+	break;
 	}
 }
