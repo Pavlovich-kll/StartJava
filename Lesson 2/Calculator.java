fff7c860b02b733352b18b70e0823c3ec43eb5a4
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-13 10:33:26 +0300

    Final fixed

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -1,27 +1,15 @@
 public class Calculator {
 
-	int number1;
-	int number2;
+	private int number1;
+	private int number2;
 	private char mathAction;
-
-	public int getNumber1() {
-		return number1;
-	}
 
 	void setNumber1(int number1) {
 		this.number1 = number1;
 	}
 
-	public int getNumber2() {
-		return number2;
-	}
-
 	void setNumber2( int number2) {
 		this.number2 = number2;
-	}
-
-	public char getMathAction() {
-		return mathAction;
 	}
 
 	void setMathAction(char mathAction) {
--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -6,23 +6,25 @@ public class CalculatorTest {
 		Scanner scan = new Scanner(System.in);
 		String answer;
 
-		System.out.println("Введите первое число: ");
-		calcOne.setNumber1(scan.nextInt());
+		do {
+			System.out.println("Введите первое число: ");
+			calcOne.setNumber1(scan.nextInt());
 
-		System.out.println("Введите знак математической операции: ");
-		calcOne.setMathAction(scan.next().charAt(0));
+			System.out.println("Введите знак математической операции: ");
+			calcOne.setMathAction(scan.next().charAt(0));
 
-		System.out.println("Введите второе число: ");
-		calcOne.setNumber2(scan.nextInt());
+			System.out.println("Введите второе число: ");
+			calcOne.setNumber2(scan.nextInt());
 
-		System.out.println("Ответ: "+ calcOne.calculate());
-		
-		do{
-			System.out.println("Хотите продолжить?[yes/no] ");
-			answer = scan.next();
-			if(answer.equals("yes") || answer.equals("no")) {
-				break;
-			}
-		} while(answer.equals(answer));
+			System.out.println("Ответ: " + calcOne.calculate());
+			do {
+				System.out.println("Хотите продолжить?[yes/no] ");
+				answer = scan.next();
+				if(answer.equals("no")) {
+					break;
+				}
+			} while(!answer.equals("yes"));
+		} while(answer.equals("yes"));
 	}
 }
+
