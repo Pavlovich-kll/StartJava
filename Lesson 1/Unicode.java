c0ca9e4b8ece6b17abea5702fe27b36437540dbf
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-17 11:45:50 +0300

    removed Lesson1 Lesson2

--- Lesson 1/Unicode.java
+++
@@ -1,7 +0,0 @@
-public class Unicode {
-	public static void main(String[] args) {
-		for(int a = 33; a <= 126; a++) {
-			System.out.println("В таблице Unicode символ с номером " + a + " это " + (char) a);
-		}
-	}
-}
