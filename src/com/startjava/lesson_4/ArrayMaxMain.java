82d40594a67c88093a30b462cbf3f05cf5c02efe
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-18 18:16:31 +0300

    Training of arrays

---
+++ src/com/startjava/lesson_4/ArrayMaxMain.java
@@ -0,0 +1,29 @@
+package com.startjava.lesson_4;
+
+import java.util.Scanner;
+
+public class ArrayMaxMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите длину массива: ");
+        int maxLenght = scanner.nextInt();
+        int[] numbers = new int[maxLenght];
+
+        System.out.print("Заполните массив целыми числами через пробел: ");
+
+        for (int i = 0; i < maxLenght; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int max = numbers[0];
+        for (int i = 0; i < maxLenght - 1; i++) {
+            if (max < numbers[i + 1]) {
+                max = numbers[i + 1];
+            }
+        }
+
+        System.out.println("Максимальное число равно = " + max);
+    }
+}
---
+++ src/com/startjava/lesson_4/ReverseOrder.java
@@ -0,0 +1,25 @@
+package com.startjava.lesson_4;
+
+public class ReverseOrder {
+    public static void main(String[] args) {
+
+        String[] a = {" каждый "," охотник "," желает ", " знать ", " где ", " сидит ", " фазан "};
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+
+        System.out.println();
+
+        String temp;
+        int n = a.length;
+        for(int i = 0; i < n/2; i++) {
+            temp = a[n-i-1];
+            a[n-i-1] = a[i];
+            a[i]= temp;
+        }
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+    }
+}
