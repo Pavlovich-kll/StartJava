6e814506fb86f2787dd7244b5df6ef65e78de0e6
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-26 12:42:03 +0300

    training

---
+++ src/com/startjava/lesson_4/training/Avarage.java
@@ -0,0 +1,28 @@
+package com.startjava.lesson_4.training;
+
+import java.util.Scanner;
+
+public class Avarage {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Введите длину массива: ");
+        int length = scan.nextInt();
+        int[] numbers = new int[length];
+
+        System.out.print("Введите числа через пробел: ");
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            numbers[i] = scan.nextInt();
+            sum = sum + numbers[i];
+        }
+
+        float average = sum / length;
+        System.out.println("Среднее арифметическое элементов массива: " + average);
+        for (int i = 0; i < length; i++) {
+           if(numbers[i] < average) {
+               System.out.println("Элементы массива, меньше среднего арифметического их суммы: " + numbers[i]);
+           }
+        }
+    }
+}
---
+++ src/com/startjava/lesson_4/training/SumEvenNumbers.java
@@ -0,0 +1,29 @@
+package com.startjava.lesson_4.training;
+
+import java.util.Scanner;
+
+public class SumEvenNumbers {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Введите длину массива: ");
+        int length = scan.nextInt();
+        int[] numbers = new int[length];
+
+        System.out.print("Заполните массив четными и нечетными числами через пробел: ");
+
+        for (int i = 0; i < length; i++) {
+            numbers[i] = scan.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            if(numbers[i] % 2 == 0 && numbers[i] >= 0) {
+                sum = sum + numbers[i];
+            }
+        }
+
+        System.out.println("Сумма четных положительных чисел равна: " + sum);
+    }
+}
