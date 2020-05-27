fa24457c64c3ece796ff3007d5614cf8923f9f64
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-27 23:11:03 +0300

    fixed method and refactoring

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class GuessNumber {
 	private Player pl1;
 	private Player pl2;
-	private int num1;
-	private int num2;
 	private int compNum = (int) (Math.random() * (100 + 1));
 	Scanner scan = new Scanner(System.in);
 
@@ -20,26 +18,26 @@ public class GuessNumber {
 		int i1 = 0;
 		int i2 = 0;
 		do {
-			System.out.println("Ходит игрок " + pl1.getName());
-			num1 = scan.nextInt();
-			pl1.setEnteredNum(num1);
-			if (num1 == compNum) {
+			setValue(pl1);
+			System.out.println(Arrays.toString(pl1.getEnteredNums()));
+			System.out.println(pl1.getEnteredNum());
+			if (pl1.getEnteredNum() == compNum) {
 				winOfPlayer(i1, pl1);
 				i2 = i2 - 1;
 				break;
 			}
 			else if (i1 == 9) {
 				System.out.println("У игрока " + pl1.getName() + " число попыток закончилось!");
-			} else if (num1 < compNum) {
+			} else if (pl1.getEnteredNum() < compNum) {
 				System.out.println(pl1.getName() + " увеличь число!");
-			} else if (num1 > compNum) {
+			} else if (pl1.getEnteredNum() > compNum) {
 				System.out.println(pl1.getName() + " уменьши число!");
 			}
 			i1++;
-			System.out.println("Ходит игрок " + pl2.getName());
-			num2 = scan.nextInt();
-			pl2.setEnteredNum(num2);
-			if (num2 == compNum) {
+			setValue(pl2);
+			System.out.println(Arrays.toString(pl2.getEnteredNums()));
+			System.out.println(pl2.getEnteredNum());
+			if (pl2.getEnteredNum() == compNum) {
 				winOfPlayer(i2, pl2);
 				i1 = i1 - 1;
 				break;
@@ -47,15 +45,21 @@ public class GuessNumber {
 				System.out.println("У игрока " + pl2.getName() + " число попыток закончилось!");
 				i1 = i1 - 1;
 				break;
-			} else if (num2 < compNum) {
+			} else if (pl2.getEnteredNum() < compNum) {
 				System.out.println(pl2.getName() + " увеличь число!");
-			} else if (num2 > compNum) {
+			} else if (pl2.getEnteredNum() > compNum) {
 				System.out.println(pl2.getName() + " уменьши число!");
 			}
 			i2++;
 		} while (i1 <= 9 && i2 <= 9);
-		copyAndZeroingOfArray(i1, pl1, num1);
-		copyAndZeroingOfArray(i2, pl2, num2);
+		copyAndZeroingOfArray(i1, pl1);
+		copyAndZeroingOfArray(i2, pl2);
+	}
+
+	public void setValue(Player pl1) {
+		System.out.println("Ходит игрок " + pl1.getName());
+		int num1 = scan.nextInt();
+		pl1.setEnteredNum(num1);
 	}
 
 	public void winOfPlayer(int i2, Player pl2) {
@@ -63,7 +67,7 @@ public class GuessNumber {
 		System.out.println("Игрок " + pl2.getName() + " угадал число " + compNum + " с " + (i2 + 1) + " попытки.");
 	}
 
-	public void copyAndZeroingOfArray(int i1, Player pl1, int num1) {
+	public void copyAndZeroingOfArray(int i1, Player pl1) {
 		int[] copySaveNumb1 = Arrays.copyOf(pl1.getEnteredNums(), (i1 + 1));
 		System.out.println("Введенные игроком " + pl1.getName() + " числа: " + Arrays.toString(copySaveNumb1));
 		Arrays.fill(copySaveNumb1, 0);
